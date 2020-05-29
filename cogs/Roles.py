3c34211fefd6e08473048a7b265394e2d5ffba37
vermoot <Vermoot@gmail.com>
2020-05-29 19:30:35 +0200

    décentralisation du spellcheck des rôles et mise en place dans les commandes

--- cogs/Roles.py
+++ cogs/Roles.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 from difflib import SequenceMatcher
 
-bot = commands.Bot(command_prefix='!')
 
 
 def isSimilar(str1, str2):
@@ -13,6 +12,44 @@ class RolesCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def getRole(self, ctx, userInput, scope):
+
+        for role in scope:
+
+            # Si le rôle existe (indépendant de la casse)
+            if userInput.lower() == role.name.lower():
+                return role
+
+        for role in scope:
+
+            # Si un rôle ressemblant existe
+            if isSimilar(userInput, role.name):
+                def check(m):
+                    return m.author == ctx.author
+                await ctx.send("Est-ce que tu veux dire \"%s\" ?" % role.name)
+                while True:
+                    answer = await self.bot.wait_for("message", check=check, timeout=10)
+                    if answer.content.lower().startswith("oui"):
+                        return role
+                    elif answer.content.lower().startswith("non"):
+                        await ctx.send("Donc tu veux bien dire %s ?" % userInput)
+                        while True:
+                            answer = await self.bot.wait_for("message", check=check, timeout=10)
+                            if answer.content.lower().startswith("oui"):
+                                return userInput
+                            elif answer.content.lower().startswith("non"):
+                                await ctx.send("Ouais ben reviens me voir quand tu seras décidé alors hein. Merde.")
+                                return None
+                            else:
+                                await ctx.send("Oui ou non, bordel.")
+                    else:
+                        await ctx.send("Oui ou non, bordel.")
+
+        # Si le rôle n'existe pas
+        return userInput
+
+
+
     @commands.group(name="role")
     async def role(self, ctx):
         if ctx.invoked_subcommand is None:
@@ -20,139 +57,63 @@ class RolesCog(commands.Cog):
 
     @role.command(name="add")
     async def addRole(self, ctx, *, roleName):
-        guild = ctx.guild
-        roleList = guild.roles
-        for role in roleList:
-
-            # Si le rôle demandé existe (insensible à la casse)
-            if roleName.lower() == role.name.lower():
-                if ctx.author in role.members:
-                    await ctx.send("Tu as déjà le rôle %s, couillon." % role.name)
-                    return
-                else:
-                    await ctx.author.add_roles(role)
-                    await ctx.send(
-                        "Voilà %s, tu as maintenant le rôle %s ! Vous êtes %i. Je crois. Mais je suis pas fort en math." % (
-                            ctx.author.display_name, role.name, len(role.members)))
-                    return
-
-            # Si le rôle demandé ressemble à un rôle qui existe
-            elif isSimilar(roleName, role.name):
-                def check(m):
-                    return m.author == ctx.author
-
-                await ctx.send("Est-ce que tu veux dire \"%s\" ?" % role.name)
-
-                # Oui c'est bien ce que je voulais dire
-                while True:
-                    answer = await bot.wait_for("message", check=check, timeout=10)
-                    if answer.content.lower().startswith("oui"):
-                        if ctx.author in role.members:
-                            await ctx.send("Tu as déjà le rôle %s, couillon." % role.name)
-                            return
-                        else:
-                            await ctx.author.add_roles(role)
-                            await ctx.send(
-                                "Alors ok, tu as maintenant le rôle %s ! Vous êtes %i. Je crois. Mais je suis pas fort en math." % (
-                                    role.name, len(role.members)))
-                            return
-
-                    # Non c'est pas ce que je voulais dire
-                    elif answer.content.lower().startswith("non"):
-                        await ctx.send("Alors tu veux effectivement le rôle \"%s\" ?" % roleName)
-                        while True:
-                            answer = await bot.wait_for("message", check=check, timeout=10)
-
-                            # Oui je veux précisément le rôle que j'ai demandé au début même s'il ressemble à un autre
-                            if answer.content.lower().startswith("oui"):
-                                newRole = await ctx.guild.create_role(name=roleName, mentionable=True)
-                                await ctx.author.add_roles(newRole)
-                                await ctx.send(
-                                    "Ok, tu l'auras voulu. Tu as maintenant le rôle %s. Vous êtes %i. Je crois. Mais je suis pas fort en math." % (
-                                    newRole.name, len(newRole.members)))
-                                return
-
-                            # Non je ne veux pas le rôle demandé au départ
-                            elif answer.content.lower().startswith("non"):
-                                await ctx.send("Ouais ben reviens me voir quand tu seras décidé alors hein. Merde.")
-                                return
-                            else:
-                                await ctx.send("Oui ou non, bordel.")
-                    else:
-                        await ctx.send("Oui ou non, bordel.")
-
-        # Si le rôle demandé ne ressemble pas à un rôle pré-existant
-        newRole = await ctx.guild.create_role(name=roleName, mentionable=True)
-        await ctx.author.add_roles(newRole)
-        await ctx.send("Voilà %s, tu as maintenant le rôle %s ! Tu es le premier, invite tes copains!" % (
-            ctx.author.display_name, newRole.name))
+        newRole = await self.getRole(ctx, roleName, ctx.guild.roles)
+        if newRole is None:
+            return
+        elif isinstance(newRole, str):
+            role = await ctx.guild.create_role(name=newRole, mentionable=True)
+            await ctx.author.add_roles(role)
+            await ctx.send("Voilà %s, tu as maintenant le rôle %s ! Tu es le premier, invite tes copains!" % (
+                ctx.author.display_name, role.name))
+        elif ctx.author in newRole.members:
+            await ctx.send("Tu as déjà le rôle %s, couillon." % newRole.name)
+            return
+        else:
+            await ctx.author.add_roles(newRole)
+            await ctx.send(
+                "Voilà %s, tu as maintenant le rôle %s ! Vous êtes %i. Je crois. Mais je suis pas fort en math." % (
+                    ctx.author.display_name, newRole.name, len(newRole.members)))
+            return
 
     @role.command(name="remove")
     async def remRole(self, ctx, *, roleName):
-        guild = ctx.guild
-        userRoles = ctx.author.roles
-        for role in userRoles:
+        roleToRemove = await self.getRole(ctx, roleName, ctx.author.roles)
+        if roleToRemove is None:
+            return
+        elif roleToRemove in ctx.author.roles:
+            await ctx.author.remove_roles(roleToRemove)
+            if len(roleToRemove.members) == 0:
+                await roleToRemove.delete()
+                await ctx.send(
+                    "Voilà %s, tu n'as maintenant plus le rôle %s. Plus personne ne l'a, d'ailleurs. Je le ferme." % (
+                        ctx.author.display_name, roleToRemove.name))
+                return
+            else:
+                await ctx.send(
+                    "Voilà %s, tu n'as maintenant plus le rôle %s." % (ctx.author.display_name, roleToRemove.name))
+                return
+        else:
+            await ctx.send("Tu n'as pas le rôle %s, andouille." % roleToRemove)
 
-            # Si la personne a effectivement ce rôle
-            if roleName.lower() == role.name.lower():
-                await ctx.author.remove_roles(role)
-                if len(role.members) == 0:
-                    await role.delete()
-                    await ctx.send(
-                        "Voilà %s, tu n'as maintenant plus le rôle %s. Plus personne ne l'a, d'ailleurs. Je le ferme." % (
-                            ctx.author.display_name, role.name))
-                    return
-                else:
-                    await ctx.send(
-                        "Voilà %s, tu n'as maintenant plus le rôle %s." % (ctx.author.display_name, role.name))
-                    return
-
-            # Si la personne a un rôle qui ressemble
-            elif isSimilar(roleName, role.name):
-
-                def check(m):
-                    return m.author == ctx.author
-
-                await ctx.send("Est-ce que tu veux dire \"%s\" ?" % role.name)
-                while True:
-                    answer = await bot.wait_for("message", check=check, timeout=10)
-                    # Oui c'est bien ce que je voulais dire
-                    if answer.content.lower().startswith("oui"):
-                        await ctx.author.remove_roles(role)
-                        if len(role.members) == 0:
-                            await role.delete()
-                            await ctx.send(
-                                "Voilà %s, tu n'as maintenant plus le rôle %s. Plus personne ne l'a, d'ailleurs. Je le ferme." % (
-                                    ctx.author.display_name, role.name))
-                        else:
-                            await ctx.send(
-                                "Voilà %s, tu n'as maintenant plus le rôle %s." % (ctx.author.display_name, role.name))
-                        return
-
-                    # Non c'est pas ce que je voulais dire
-                    elif answer.content.lower().startswith("non"):
-                        await ctx.send("Bon ben j'ai pas bien compris alors. Exprime toi mieux.")
-                        return
-                    else:
-                        await ctx.send("Oui ou non, bordel.")
-
-        # Si la personne n'a pas de rôle qui ressemble
-        await ctx.send("Tu n'as pas le rôle %s, andouille." % roleName)
 
     @role.command(name="list")
     async def listRoles(self, ctx, *, role=None):
-        guild = ctx.guild
         if role is None:
             rolesList = ""
             for role in ctx.guild.roles:
                 rolesList = rolesList + ("%s (%i membres)\n" % (role.name, len(role.members)))
             await ctx.send("Voici la liste des rôles qui existent ici :\n```\n%s\n```" % str(rolesList))
         else:
-            membersList = ""
-            inRole = discord.utils.get(guild.roles, name=role)
-            for member in inRole.members:
-                membersList = membersList + member.display_name + "\n"
-            await ctx.send("Voici la liste des gens dans %s :\n```\n%s\n```" % (inRole, membersList))
+            inRole = await self.getRole(ctx, role, ctx.guild.roles)
+            if isinstance(inRole, str):
+                await ctx.send("Le rôle %s n'existe pas." % inRole)
+            elif inRole is None:
+                return
+            else:
+                membersList = ""
+                for member in inRole.members:
+                    membersList = membersList + member.display_name + "\n"
+                await ctx.send("Voici la liste des gens dans %s :\n```\n%s\n```" % (inRole.name, membersList))
 
     @role.command(name="ass")
     async def ass(self, ctx):
