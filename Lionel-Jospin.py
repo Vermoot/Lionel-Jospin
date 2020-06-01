0bd9b0c68376571d43cf445ea0778d5a604eb9be
vermoot <Vermoot@gmail.com>
2020-06-01 13:21:59 +0200

    Started working on reaction menus

--- Lionel-Jospin.py
+++ Lionel-Jospin.py
@@ -24,4 +24,12 @@ async def repeat(ctx, *, message):
     await ctx.send(message)
 
 
+@bot.command(name="reload")
+async def reload(ctx, cog):
+    print(ctx.author.id)
+    await bot.unload_extension(cog)
+    await bot.load_extension(cog)
+    await ctx.send("Cog \"%s\n reloaded!")
+
+
 bot.run(TOKEN)
--- cogs/Lionel.py
+++ cogs/Lionel.py
@@ -1,11 +1,30 @@
 import discord
 from discord.ext import commands
 import random
+# from Utilities import reaction_menu
+
 
 
 class LionelCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    async def reaction_menu(self, ctx, question, *choices: str):
+        choice_number = 1
+        message_text = question + "```"
+        for choice in list(choices):
+            message_text += "%s️⃣ %s \n" % (choice_number, choice)
+            choice_number += 1
+        message_text += "```"
+        message = await ctx.send(message_text)
+        reaction_number = 1
+        for choice in list(choices):
+            await message.add_reaction("%s️⃣" % reaction_number)
+            reaction_number += 1
+        def check(reaction, user):
+            return user == ctx.author
+        reaction = await self.bot.wait_for("reaction_add", check=check, timeout=None)
+        return [list(reaction)[0].emoji[0], choices[int(list(reaction)[0].emoji[0])-1]]
 
     @commands.group(name="lionel")
     async def lionel(self, ctx):
@@ -22,6 +41,13 @@ class LionelCog(commands.Cog):
     async def help(self, ctx):
         discord.ext.commands.HelpCommand(ctx)
 
+    @commands.command(name="testmenu")
+    async def test_menu(self, ctx):
+        choice = await self.reaction_menu(ctx, "Choisis une option :",  "La première",
+                                                                        "Peut-être la deuxième",
+                                                                        "Qui sait, pourquoi pas la troisième",
+                                                                        "La réponse D")
+        await ctx.send("Tu as choisi l'option %s : %s" % (choice[0], choice[1]))
 
 def setup(bot):
     bot.add_cog(LionelCog(bot))
